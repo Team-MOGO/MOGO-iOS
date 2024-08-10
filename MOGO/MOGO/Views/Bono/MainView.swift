728c90a2c0014fc72a9a8494132507a837a9c1ef
bono <shippingbok@gmail.com>
2024-08-11 05:53:58 +0900

    부링부리!! 정션 마지막 날이야!

--- MOGO/MOGO/Views/Bono/MainView.swift
+++ MOGO/MOGO/Views/Bono/MainView.swift
@@ -13,163 +13,183 @@ struct MainView: View {
   @State private var isShowingScanner = false
   @State private var rotationAngle: Double = 0
   @State private var selectedRochelleIdx: Int = 0
+  @State private var analyzingItem: Bool = false
   let babyTalk = BabyTalk.random()
   
   var body: some View {
-    let state = PregnantState
+    let state = BabyViewState
       .get(pregnantdays: pregnantDays)
-    ZStack {
-      VStack(spacing: 0) {
-        HStack(alignment: .bottom, spacing: 8) {
-          Text("Week \(PregnantState.getWeek(pregnantdays: pregnantDays))")
-            .mogoFont(.urbanistBold30)
-            .foregroundStyle(.mogoPoint)
-          
-          Text("\(280-pregnantDays) days to go")
-            .mogoFont(.urbanistMedium16)
-            .foregroundStyle(.mogoPoint)
-            .padding(.bottom, 3)
-          Spacer()
-        }
-        .padding(.horizontal, 30)
-        .padding(.top, 32)
-        
-        HStack(spacing: -10) {
-          Button(
-            action: {
-              selectedRochelleIdx = (selectedRochelleIdx + 2) % 3
-            },
-            label: {
-              Image(systemName: "chevron.backward")
-                .frame(width: 25, height: 25)
-            }
-          )
-          .offset(x: 30)
-          .zIndex(1)
-          
-          state.rochelleBox[selectedRochelleIdx]
-            .resizable()
-            .scaledToFit()
-            .frame(height: 300)
-          
-          Button(
-            action: {
-              selectedRochelleIdx = (selectedRochelleIdx + 1) % 3
-            },
-            label: {
-              Image(systemName: "chevron.right")
-                .frame(width: 25, height: 25)
-            }
-          )
-          .offset(x: -30)
-        }
-        .tint(.mogoGray3)
-        
-        
-        Button(
-          action: {
-            isShowingScanner = true
-          },
-          label: {
-            RoundedRectangle(cornerRadius: 30)
-              .fill(.mogoPoint)
-              .frame(width: 174, height: 52)
-              .overlay {
-                ZStack {
-                  Text("Scan")
-                    .mogoFont(.urbanistBold18)
-                    .foregroundStyle(.white)
-                  HStack {
-                    Spacer()
-                    Circle()
-                      .fill(.white)
-                      .opacity(0.18)
-                      .frame(width: 31)
-                      .overlay {
-                        Image(.barcode)
-                      }
-                      .padding(.trailing, 10)
-                  }
-                }
-              }
-          }
-        )
-        
-        Spacer()
-        
-      }
-    }
-    
-    .background {
-      VStack(spacing: 0) {
-        
-        Spacer()
-        
-        Image(.quote)
-          .padding(.bottom, 7)
-        
-        Text(babyTalk)
-          .multilineTextAlignment(.center)
-          .mogoFont(.ramblaBold16)
-          .foregroundStyle(.mogoPoint)
-          .padding(.bottom, 19)
-        
-        HStack(alignment: .bottom) {
-          getMothersStomachImage(state)
-            .overlay(alignment: .bottom) {
-              getBabyImage(state)
-                .rotationEffect(Angle(degrees: rotationAngle), anchor: .center)
-                .animation(
-                  Animation.easeInOut(duration: 2.0) // 느리게 애니메이션 설정 (1초)
-                    .repeatForever(), value: rotationAngle
-                )
-                .onAppear {
-                  rotationAngle = 10 // 회전 각도 설정
-                }
-            }
-        }
-      }
-      .ignoresSafeArea(edges: .bottom)
-      .sheet(isPresented: $isShowingScanner) {
-        ZStack(alignment: .top) {
-          BarcodeScannerView { code in
-            self.scannedCode = code
-            self.isShowingScanner = false
-          }
-          
-          HStack {
-            Button(
-              action: {
-                isShowingScanner = false
-              },
-              label: {
-                Rectangle()
-                  .fill(.clear)
-                  .scaledToFit()
-                  .frame(width: 24)
-                  .overlay {
-                    Image(systemName: "xmark")
-                      .resizable()
-                      .scaledToFit()
-                      .frame(width: 16)
-                      .tint(.white)
-                  }
-              }
-            )
+    if !analyzingItem {
+      ZStack {
+        VStack(spacing: 0) {
+          HStack(alignment: .bottom, spacing: 8) {
+            Text("Week \(BabyViewState.getWeek(pregnantdays: pregnantDays))")
+              .mogoFont(.urbanistBold30)
+              .foregroundStyle(.mogoPoint)
             
+            Text("\(PregnantState.pregnantFullDays-pregnantDays) days to go")
+              .mogoFont(.urbanistMedium16)
+              .foregroundStyle(.mogoPoint)
+              .padding(.bottom, 3)
             Spacer()
           }
-          .padding(.all, 40)
+          .padding(.horizontal, 30)
+          .padding(.top, 32)
           
-          Text("Please scan the barcode of the product")
-            .mogoFont(.urbanistBold17)
-            .foregroundColor(.white)
-            .padding(.top, 97)
+          HStack(spacing: -10) {
+            Button(
+              action: {
+                selectedRochelleIdx = (selectedRochelleIdx + 2) % 3
+              },
+              label: {
+                Image(systemName: "chevron.backward")
+                  .frame(width: 25, height: 25)
+              }
+            )
+            .offset(x: 30)
+            .zIndex(1)
+            
+            state.rochelleBox[selectedRochelleIdx]
+              .resizable()
+              .scaledToFit()
+              .frame(height: 300)
+            
+            Button(
+              action: {
+                selectedRochelleIdx = (selectedRochelleIdx + 1) % 3
+              },
+              label: {
+                Image(systemName: "chevron.right")
+                  .frame(width: 25, height: 25)
+              }
+            )
+            .offset(x: -30)
+          }
+          .tint(.mogoGray3)
+          
+          
+          Button(
+            action: {
+              isShowingScanner = true
+            },
+            label: {
+              RoundedRectangle(cornerRadius: 30)
+                .fill(.mogoPoint)
+                .frame(width: 174, height: 52)
+                .overlay {
+                  ZStack {
+                    Text("Scan")
+                      .mogoFont(.urbanistBold18)
+                      .foregroundStyle(.white)
+                    HStack {
+                      Spacer()
+                      Circle()
+                        .fill(.white)
+                        .opacity(0.18)
+                        .frame(width: 31)
+                        .overlay {
+                          Image(.barcode)
+                        }
+                        .padding(.trailing, 10)
+                    }
+                  }
+                }
+            }
+          )
+          
+          Spacer()
+          
         }
       }
-      .background(
-        .mogoBackground
-      )
+      .background {
+        VStack(spacing: 0) {
+          
+          Spacer()
+          
+          Image(.quote)
+            .padding(.bottom, 7)
+          
+          Text(babyTalk)
+            .multilineTextAlignment(.center)
+            .mogoFont(.ramblaBold16)
+            .foregroundStyle(.mogoPoint)
+            .padding(.bottom, 19)
+          
+          HStack(alignment: .bottom) {
+            getMothersStomachImage(state)
+              .overlay(alignment: .bottom) {
+                getBabyImage(state)
+                  .rotationEffect(Angle(degrees: rotationAngle), anchor: .center)
+                  .animation(
+                    Animation.easeInOut(duration: 2.0) // 느리게 애니메이션 설정 (1초)
+                      .repeatForever(), value: rotationAngle
+                  )
+                  .onAppear {
+                    rotationAngle = 10 // 회전 각도 설정
+                  }
+              }
+          }
+        }
+        .ignoresSafeArea(edges: .bottom)
+        .sheet(isPresented: $isShowingScanner) {
+          ZStack(alignment: .top) {
+            BarcodeScannerView { code in
+              self.scannedCode = code
+              self.analyzingItem = true
+              self.isShowingScanner = false
+            }
+            
+            HStack {
+              Button(
+                action: {
+                  isShowingScanner = false
+                },
+                label: {
+                  Rectangle()
+                    .fill(.clear)
+                    .scaledToFit()
+                    .frame(width: 24)
+                    .overlay {
+                      Image(systemName: "xmark")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 16)
+                        .tint(.white)
+                    }
+                }
+              )
+              
+              Spacer()
+            }
+            .padding(.all, 40)
+            
+            Text("Please scan the barcode of the product")
+              .mogoFont(.urbanistBold17)
+              .foregroundColor(.white)
+              .padding(.top, 97)
+          }
+        }
+        .background(
+          .mogoBackground
+        )
+      }
+    } else {
+      if let scannedCode {
+        let itemBarcodeIdentity =  scannedCode.prefix(12)
+        let allData: [ResultItem] = ResultItem.dummy
+        
+        if let selectedItem = allData.first(where: { String($0.productCode).prefix(12) == itemBarcodeIdentity && ($0.pregnancyStage == PregnantState.get(pregnantdays: pregnantDays).rawValue || $0.pregnancyStage == 0) }) {
+          
+          ResultView(selectedItem: selectedItem)
+        } else {
+          Text("정보가 없습니다")
+        }
+      } else {
+        Text("정보가 없습니다")
+          .task {
+            analyzingItem = false
+          }
+      }
     }
   }
 }
--- MOGO/MOGO/Views/Brea/ResultView.swift
+++ MOGO/MOGO/Views/Brea/ResultView.swift
@@ -8,94 +8,74 @@
 import SwiftUI
 
 struct ResultView: View {
-  var resultItems: [ResultItem]
-  var productCode: Int
-  var pregnancyStage: Int
-  
-  @State private var dynamicWidth: CGFloat = 100
+  let selectedItem: ResultItem
   
   var body: some View {
-    if let selectedItem = resultItems.first(where: { $0.productCode == productCode && $0.pregnancyStage == pregnancyStage }) {
-      
-      ScrollView {
-        VStack(spacing: 0) {
-          HStack(spacing: 0) {
-            riskLevelImage(for: selectedItem.riskLevel)
-              .resizable()
-              .scaledToFit()
-              .frame(width: 132)
+    
+    ScrollView {
+      VStack(spacing: 0) {
+        HStack(spacing: 0) {
+          riskLevelImage(for: selectedItem.riskLevel)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 132)
+          
+          VStack(alignment: .leading, spacing: 0) {
+            Text("\" \(selectedItem.foodName) \"")
+              .mogoFont(.urbanistSemiBold18)
+              .padding(.bottom, 5)
+              .padding(.leading, 13)
             
-            VStack(alignment: .leading, spacing: 0) {
-              Text("\" \(selectedItem.foodName) \"")
-                .mogoFont(.urbanistSemiBold18)
-                .padding(.bottom, 5)
-                .padding(.leading, 13)
-              
-              VStack(alignment: .leading, spacing: 4) {
-                HStack(spacing: 0) {
-                  Text("is ")
-                    .mogoFont(.urbanistBold22)
-                  
-                  Text(selectedItem.riskLevel.capitalized)
-                    .mogoFont(.urbanistBold22)
-                    .foregroundStyle(riskLevelColor(for: selectedItem.riskLevel))
-                }
+            VStack(alignment: .leading, spacing: 4) {
+              HStack(spacing: 0) {
+                Text("is ")
+                  .mogoFont(.urbanistBold22)
                 
-                Text("at week \(selectedItem.pregnancyStage) pregnancy")
-                  .mogoFont(.urbanistBold20)
-                  .lineLimit(1)
-                  .frame(maxWidth: .infinity, alignment: .leading)
+                Text(selectedItem.riskLevel.capitalized)
+                  .mogoFont(.urbanistBold22)
+                  .foregroundStyle(riskLevelColor(for: selectedItem.riskLevel))
               }
-              .padding(.leading, 20)
-              .padding(.bottom, 14)
               
-              riskLevelCauseView(for: selectedItem)
-                .padding(.leading, 20)
-                .padding(.trailing, 2)
+              Text("at week \(selectedItem.pregnancyStage) pregnancy")
+                .mogoFont(.urbanistBold20)
+                .lineLimit(1)
+                .frame(maxWidth: .infinity, alignment: .leading)
             }
+            .padding(.leading, 20)
+            .padding(.bottom, 14)
+            
+            riskLevelCauseView(for: selectedItem)
+              .padding(.leading, 20)
+              .padding(.trailing, 2)
           }
-          .padding(.bottom, 43)
+        }
+        .padding(.bottom, 43)
+        
+        HStack() {
+          Text(selectedItem.cause)
+            .mogoFont(.urbanistBold20)
+          Spacer()
+        }
+        .padding(.horizontal, 21)
+        .padding(.bottom, 12)
+        
+        ZStack(alignment: .center) {
+          VStack(spacing: 0) {
+            RoundedRectangle(cornerRadius: 4)
+              .fill(Color(hex: "C6C6C6"))
+              .frame(width: 2, height: 35)
+              .padding(.bottom, 9)
+            Text("Ideal daily amount")
+              .mogoFont(.pretendardMedium12)
+          }
           
-          HStack() {
-            Text(selectedItem.cause)
-              .mogoFont(.urbanistBold20)
+          VStack(spacing: 0) {
             Spacer()
-          }
-          .padding(.horizontal, 21)
-          .padding(.bottom, 12)
-          
-          ZStack(alignment: .bottom) {
-            RoundedRectangle(cornerRadius: 20)
-              .fill(.white)
-              .stroke(Color(hex: "C4C3C3"), lineWidth: 0)
-              .frame(height: 117)
-              .overlay(
-                VStack(spacing: 0) {
-                  RoundedRectangle(cornerRadius: 4)
-                    .fill(Color(hex: "C6C6C6"))
-                    .frame(width: 2, height: 35)
-                    .padding(.bottom, 9)
-                  Text("Ideal daily amount")
-                    .mogoFont(.pretendardMedium12)
-                }
-              )
-            
-            HStack(spacing: 0) {
-              Spacer()
-              Text("*per 100g")
-                .mogoFont(.pretendardLight12)
-                .padding(.trailing, 14)
-                .padding(.bottom, 13)
-            }
-            
-            // MARK: - 보노 계산해조 나 못하겟어
-            VStack(spacing: 0) {
-              Spacer()
-              ZStack(alignment: .leading) {
+            ZStack(alignment: .leading) {
+              GeometryReader(content: { geometry in
                 RoundedRectangle(cornerRadius: 10)
-                  .fill(Color(hex: "F7F0EE"))
                   .frame(height: 16)
-                  .padding(.horizontal, 22)
+                
                 RoundedRectangle(cornerRadius: 10)
                   .fill(
                     LinearGradient(
@@ -104,99 +84,109 @@ struct ResultView: View {
                       endPoint: .trailing
                     )
                   )
-                  .frame(width: 210, height: 16)
-                  .padding(.horizontal, 22)
+                  .frame(height: 16)
+                  .frame(width: geometry.size.width * calculateRatioOfGraph(width: geometry.size.width))
               }
-              Spacer()
+              )
+              .padding(.horizontal, 22)
             }
-            .offset(y: -12)
-          }
-          .padding(.horizontal, 19)
-          .padding(.bottom, 17)
-          
-          Text(selectedItem.description)
-            .mogoFont(.pretendardRegular16)
-            .padding(.horizontal, 20)
-            .padding(.bottom, 40)
-          
-          HStack(spacing: 0) {
-            Text("Recommended Intake Per Day")
-              .mogoFont(.urbanistBold20)
             Spacer()
           }
-          .padding(.leading, 20)
-          .padding(.bottom, 12)
-          
+        }
+        .padding(.top, 34)
+        .padding(.bottom, 24)
+        .background(
+          RoundedRectangle(cornerRadius: 20)
+            .fill(.white)
+        )
+        .padding(.horizontal, 19)
+        .padding(.bottom, 17)
+        
+        Text(selectedItem.description)
+          .mogoFont(.pretendardRegular16)
+          .padding(.horizontal, 20)
+          .padding(.bottom, 40)
+        
+        HStack(spacing: 0) {
+          Text("Recommended Intake Per Day")
+            .mogoFont(.urbanistBold20)
+          Spacer()
+        }
+        .padding(.leading, 20)
+        .padding(.bottom, 12)
+        
+        RoundedRectangle(cornerRadius: 21)
+          .fill(.white)
+          .stroke(Color(hex: "C4C3C3"), lineWidth: 0)
+          .frame(height: 92)
+          .overlay(
+            Text(selectedItem.dailyRecommendedIntake)
+              .mogoFont(.urbanistBold24)
+              .foregroundStyle(.mogoGray2)
+          )
+          .padding(.horizontal, 22)
+          .padding(.bottom, 19)
+        
+        HStack(spacing: 0) {
+          Text("Nutrition Facts")
+            .mogoFont(.urbanistBold20)
+          Spacer()
+        }
+        .padding(.horizontal, 21)
+        .padding()
+        
+        
+        ZStack(alignment: .center) {
           RoundedRectangle(cornerRadius: 21)
             .fill(.white)
-            .stroke(Color(hex: "C4C3C3"), lineWidth: 0)
-            .frame(height: 92)
-            .overlay(
-              Text(selectedItem.dailyRecommendedIntake)
-                .mogoFont(.urbanistBold24)
-                .foregroundStyle(.mogoGray2)
-            )
+            .stroke(Color(hex: "C4C3C3"), lineWidth: 1)
+          
+          VStack(spacing: 0) {
+            HStack(spacing: 0) {
+              Text("Calories")
+                .mogoFont(.pretendardBold20)
+              
+              Spacer()
+              
+              Text("\(selectedItem.nutritionFacts.calories)kcal")
+                .mogoFont(.pretendardBold20)
+            }
             .padding(.horizontal, 22)
-            .padding(.bottom, 19)
-          
-          HStack(spacing: 0) {
-            Text("Nutrition Facts")
-              .mogoFont(.urbanistBold20)
-            Spacer()
-          }
-          .padding(.horizontal, 21)
-          .padding()
-          
-          
-          ZStack(alignment: .center) {
-            RoundedRectangle(cornerRadius: 21)
-              .fill(.white)
-              .stroke(Color(hex: "C4C3C3"), lineWidth: 1)
+            .padding(.bottom, 18)
             
-            VStack(spacing: 0) {
+            Divider()
+              .background(Color(hex: "C4C3C3"))
+              .padding(.bottom, 17)
+            
+            ForEach(selectedItem.nutritionFacts.nutritions, id: \.self) { nutrition in
               HStack(spacing: 0) {
-                Text("Calories")
-                  .mogoFont(.pretendardBold20)
+                Text(nutrition.name)
+                  .mogoFont(.pretendardMedium16)
                 
                 Spacer()
                 
-                Text("\(selectedItem.nutritionFacts.calories)kcal")
-                  .mogoFont(.pretendardBold20)
+                nutritionLevelView(for: nutrition)
+                  .padding(.trailing, 8)
+                
+                Text(nutrition.amount)
+                  .mogoFont(.pretendardMedium14)
               }
               .padding(.horizontal, 22)
-              .padding(.bottom, 18)
-              
-              Divider()
-                .background(Color(hex: "C4C3C3"))
-                .padding(.bottom, 17)
-              
-              ForEach(selectedItem.nutritionFacts.nutritions, id: \.self) { nutrition in
-                HStack(spacing: 0) {
-                  Text(nutrition.name)
-                    .mogoFont(.pretendardMedium16)
-                  
-                  Spacer()
-                  
-                  nutritionLevelView(for: nutrition)
-                    .padding(.trailing, 8)
-                  
-                  Text(nutrition.amount)
-                    .mogoFont(.pretendardMedium14)
-                }
-                .padding(.horizontal, 22)
-                .padding(.bottom, 20)
-              }
+              .padding(.bottom, 20)
             }
-            .padding(.top, 20)
           }
-          .padding(.horizontal, 19)
+          .padding(.top, 20)
         }
-        .padding(.top, 20)
+        .padding(.horizontal, 19)
+        .padding(.bottom, 40)
+        
       }
-      .ignoresSafeArea(edges: .bottom)
-      .frame(maxWidth: .infinity)
-      .background(.mogoBackground)
+      .padding(.top, 20)
+      
     }
+    .ignoresSafeArea(edges: .bottom)
+    .frame(maxWidth: .infinity)
+    .background(.mogoBackground)
   }
 }
 
@@ -272,32 +262,23 @@ extension ResultView {
         RoundedRectangle(cornerRadius: 8)
           .fill(backgroundColor)
       )
-      .padding(.leading, 20)
-      .padding(.trailing, 2)
+      .fixedSize()
+  }
+  
+  func calculateRatioOfGraph(width: Double) -> Double {
+    let idealAmount = Double(selectedItem.idealAmount) // 기준
+    let actualAmount = Double(selectedItem.actualAmount) // 실제
+    let ratio = actualAmount / idealAmount
+    switch ratio {
+    case 0...2:
+      return ratio / 2
+    default:
+      return 1
+    }
   }
 }
 
 
 #Preview {
-  ResultView(resultItems: [
-    ResultItem(productCode: 0,
-               pregnancyStage: 0,
-               foodName: "Chocolate Cereal",
-               riskLevel: "dangerous",
-               cause: "Excessive Vitamin D",
-               idealAmount: 10,
-               actualAmount: 12,
-               description: "A 15-week-old pregnant woman has a daily intake of vitamin D of 10mg. Excessive intake of vitamin D negatively affects the development of the fetus's bone and may also affect the development of the nervous system.",
-               dailyRecommendedIntake: "less than 1",
-               nutritionFacts:
-                NutritionFacts(calories: 393,
-                               nutritions: [
-                                Nutrition(name: "Carbohydrate", amount: "85g", level: "Careful"),
-                                Nutrition(name: "Protein", amount: "5g", level: "Ideal"),
-                                Nutrition(name: "Fat", amount: "3.67g", level: "Ideal"),
-                                Nutrition(name: "Sugar", amount: "30g", level: "Excessive"),
-                                Nutrition(name: "Sodium", amount: "450mg", level: "Careful"),
-                                Nutrition(name: "Vitatmin A", amount: "583mg", level: "Excessive")
-                               ]))
-  ], productCode: 0, pregnancyStage: 0)
+  ResultView(selectedItem: .dummy.first!)
 }
